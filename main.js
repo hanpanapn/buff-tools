ef6ab7e6b3aa0570bc19d3bf8978d00f8b719d78
pan <hanpanpan@outlook.com>
2022-08-02 19:12:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Buff Tools 网易Buff助手
 // @namespace    https://github.com/hanpanapn/buff-tools
-// @version      0.5
+// @version      0.6.1
 // @description  buff tools
 // @author       hanpanpan@outlook.com
 // @match        https://buff.163.com/*
